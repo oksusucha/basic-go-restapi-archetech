e3a0581415370654226d0923d17843af6dd7c75c
oksusucha <sol4877@gmail.com>
2021-10-04 15:09:32 +0900

    feat: initial route setting and inject db object

--- server/router.go
+++ server/router.go
@@ -1,1 +1,33 @@
 package server
+
+import customtask "basic_archetech/controller/v1/custom_task"
+
+type APIEvents struct {
+	CT *customtask.CustomTaskAPIEvent
+	// Please modify to your api path.
+}
+
+func newApiEvents() *APIEvents {
+	db := makeRDBMSConnection()
+
+	return &APIEvents{
+		// Please modify to your api path.
+		CT: &customtask.CustomTaskAPIEvent{DB: db},
+	}
+}
+
+func initRouter() {
+	apiEvents := newApiEvents()
+
+	// r.Use()
+
+	api := r.Group("api")
+
+	v1 := api.Group("v1")
+
+	// Please modify to your api path.
+	customTask := v1.Group("ct")
+	{
+		customTask.GET("/ct", apiEvents.CT.PleaseChangeToYourAPIServiceName)
+	}
+}
